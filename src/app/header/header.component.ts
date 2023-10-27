26706b99661b47f0c41e8e89f7ec50b9399c48c5
Saber Montassar <saber.montassar.ext@siemens.com>
2023-10-27 06:04:52 +0200

    use @Output to communicate with main cmp/ imp navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"],
 })
 export class HeaderComponent implements OnInit {
+  @Output() selectorEmitter = new EventEmitter<string>();
   ngOnInit(): void {}
+
+  selectRecipe() {
+    this.selectorEmitter.emit("recipe");
+  }
+
+  selectShopping() {
+    this.selectorEmitter.emit("shopping");
+  }
 }

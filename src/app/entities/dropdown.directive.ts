7c6c65e57f6ef69636fd47bf9cb23469b5bd0d8b
Saber Montassar <saber.montassar.ext@siemens.com>
2023-10-29 20:36:20 +0100

    add directive Dropdown

---
+++ src/app/entities/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import { SelectorContext } from "@angular/compiler";
+import { Directive, HostBinding, HostListener } from "@angular/core";
+
+@Directive({
+  selector: "[appDropdown]",
+})
+export class DropdownDirective {
+  @HostBinding("class.open") isOpen: boolean = false;
+
+  @HostListener("click") toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+}

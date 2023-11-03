5e9a5c2e3f2cb306635979d8950d08eecf43fd1d
Saber Montassar <saber.montassar.ext@siemens.com>
2023-11-04 00:02:22 +0100

    adding ingredients to recipe

--- src/app/service/recipe.service.ts
+++ src/app/service/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "../entities/recipe.model";
+import { Ingredient } from "../entities/ingredient.model";
 
 // @Injectable({
 //   providedIn: "root",
@@ -9,12 +10,14 @@ export class RecipeService {
     new Recipe(
       "A Test Recipe",
       "This is a sample description",
-      "https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg"
+      "https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg",
+      [new Ingredient("AAAA", 1), new Ingredient("BBB", 2)]
     ),
     new Recipe(
       "A Test Recipe 2",
       "This is a sample description 2",
-      "https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg"
+      "https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg",
+      [new Ingredient("CCCCC", 1), new Ingredient("DDDD", 2)]
     ),
   ];
 

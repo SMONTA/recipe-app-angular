f38b3ce7fa90bc6d6bb4c0ec00d49897d2e5d3a3
Saber Montassar <saber.montassar.ext@siemens.com>
2023-12-03 23:55:50 +0100

    add form array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from "@angular/core";
-import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 import { RecipeService } from "src/app/service/recipe.service";
 
@@ -13,6 +19,7 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
   constructor(
+    private fb: FormBuilder,
     private route: ActivatedRoute,
     private recipeService: RecipeService
   ) {}
@@ -39,19 +46,45 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = "";
     let recipeImagePath = "";
     let recipeDescription = "";
+    let recipeIngredients = this.fb.array<
+      FormGroup<{ name: FormControl<string>; amount: FormControl<number> }>
+    >([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipeById(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe["ingredients"]) {
+        recipe.ingredients.forEach((ingredient) => {
+          let ingredientCtl = this.fb.group({
+            name: [ingredient.name],
+            amount: [ingredient.amount],
+          });
+          recipeIngredients.push(ingredientCtl);
+        });
+      }
     }
-
-    this.recipeForm = new FormGroup({
-      name: new FormControl(recipeName, Validators.required),
-      imagePath: new FormControl(recipeImagePath, Validators.required),
-      description: new FormControl(recipeDescription, Validators.required),
-      ingredients: new FormArray([]),
+    this.recipeForm = this.fb.group({
+      name: [recipeName, Validators.required],
+      imagePath: [recipeImagePath, Validators.required],
+      description: [recipeDescription, Validators.required],
+      ingredients: recipeIngredients,
     });
+  }
+
+  public get ingredientsControl(): FormArray {
+    return this.recipeForm.get("ingredients") as FormArray;
+  }
+
+  removeIngredient() {}
+
+  addIngredient() {
+    this.ingredientsControl.push(
+      this.fb.group({
+        name: [],
+        amount: [],
+      })
+    );
   }
 }

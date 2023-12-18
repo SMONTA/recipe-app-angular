d574fde5321a293fa1043f627bd37a3dd7ce855c
Saber Montassar <saber.montassar.ext@siemens.com>
2023-12-18 07:10:05 +0100

    save data to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
+import { HttpClientModule, provideHttpClient } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
@@ -39,7 +39,7 @@ import { ShoppingService } from "./service/shopping.service";
     BrowserAnimationsModule,
     AppRoutingModule,
   ],
-  providers: [ShoppingService, RecipeService],
+  providers: [ShoppingService, RecipeService, provideHttpClient()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/service/data-storage.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Inject, Injectable, inject } from "@angular/core";
+import { Recipe } from "../entities/recipe.model";
+import { RecipeService } from "./recipe.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class DataStorageService {
+  private URL =
+    "https://ng-course-recipe-29586-default-rtdb.europe-west1.firebasedatabase.app/recipes.json";
+  private http = inject(HttpClient);
+
+  constructor(private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    let recipes = this.recipeService.getRecipes();
+    this.http.put(this.URL, recipes).subscribe((resp) => {
+      console.log(resp);
+    });
+  }
+
+  fetchRecipes() {
+    return this.http.get(this.URL);
+  }
+}

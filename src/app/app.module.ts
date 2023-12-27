2895f3bd16d58cfa04851fdee5b52a7b31bb820a
Saber Montassar <saber.montassar.ext@siemens.com>
2023-12-27 15:46:11 +0100

    split module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,15 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import {
   HTTP_INTERCEPTORS,
-  HttpClientModule,
   provideHttpClient,
-  withInterceptors,
   withInterceptorsFromDi,
 } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { DropdownDirective } from "./entities/dropdown.directive";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RecipeService } from "./service/recipe.service";
 import { ShoppingService } from "./service/shopping.service";
@@ -27,20 +19,15 @@ import { AuthComponent } from "./auth/auth.component";
 import { AuthInterceptorService } from "./service/auth-interceptor.service";
 import { AlertComponent } from "./shared/alert/alert.component";
 import { NgComponentOutlet } from "@angular/common";
+import { RecipesModule } from "./recipes/recipes.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeDetailComponent,
     DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AuthComponent,
     AlertComponent,
   ],
@@ -51,6 +38,7 @@ import { NgComponentOutlet } from "@angular/common";
     BrowserAnimationsModule,
     AppRoutingModule,
     NgComponentOutlet,
+    RecipesModule,
   ],
   providers: [
     ShoppingService,
